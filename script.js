3f9538d831a7eeaa2db4c6e0482441c4bc8c3319
Gabriel Campos Cristofolli Wendich <campos.wendich@gmail.com>
2022-03-02 06:53:01 -0300

    mobile done :tada:

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// hidden menu
 const nav = document.querySelector("#header nav")
 const toggle = document.querySelectorAll("nav .toggle")
 const links = document.querySelectorAll("nav .menu ul a")
@@ -16,18 +17,34 @@ for (const link of links) {
 }
 
 
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
+// header shadow
+function scrollHeadedrShadow() {
+   const header = document.querySelector("#header")
+   const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
    if (window.scrollY>=navHeight) {
       header.classList.add("scroll")
    } else {
       header.classList.remove("scroll")
    }
+}
+
+// back to top button
+function backToTop() {
+   const backToTopButton = document.querySelector(".back-to-top")
+   
+   if (window.scrollY>=560) {
+      backToTopButton.classList.add("show")
+   } else {
+      backToTopButton.classList.remove("show")
+   }
+}
+
+// scroll
+window.addEventListener('scroll', () => {
+   scrollHeadedrShadow(),
+   backToTop()
 })
-
-
 
 
 // swiper carousel
@@ -47,3 +64,38 @@ const swiper = new Swiper('.swiper', {
       onlyInViewport: false,
     },
  });
+
+
+
+// scrollreveal
+const scrollReveal = ScrollReveal({
+   delay: 0,
+   easing: 'cubic-bezier(0.5, 0, 0, 1)',
+   interval: 0,
+   opacity: 0,
+   rotate: {
+      x: 0,
+      y: 0,
+      z: 0,
+   },
+   origin: 'top',
+   distance: '30px',
+   duration: 700,
+   reset: true,
+})
+
+scrollReveal.reveal(`
+   #home .image, 
+   #home .text,
+   #about .image, 
+   #about .text,
+   #services header,
+   #services .card,
+   #testimonials header,
+   #testimonials .testimonials,
+   #contact .text,
+   #contact .links
+   `,
+   { interval: 100 })
+
+//  interval - interval between an iten and the next one
